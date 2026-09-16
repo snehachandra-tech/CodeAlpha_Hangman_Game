e04ba1801df2271ca2ef6a7af1d07ab72d4b11f5
SNEHA CHANDRA <snehachandra161105@gmail.com>
2026-09-16 13:03:25 +0530

    Update hangman.py

--- hangman.py
+++ hangman.py
@@ -1,1 +1,29 @@
+import random
 
+words = ["python", "coding", "github", "hangman"]
+
+word = random.choice(words)
+guessed = ["_"] * len(word)
+attempts = 6
+
+print("Welcome to Hangman Game!")
+
+while attempts > 0 and "_" in guessed:
+    print("\nWord:", " ".join(guessed))
+    print("Attempts left:", attempts)
+
+    guess = input("Guess a letter: ").lower()
+
+    if guess in word:
+        for i in range(len(word)):
+            if word[i] == guess:
+                guessed[i] = guess
+        print("Correct guess!")
+    else:
+        attempts -= 1
+        print("Wrong guess!")
+
+if "_" not in guessed:
+    print("You won! The word was:", word)
+else:
+    print("Game over! The word was:", word)
